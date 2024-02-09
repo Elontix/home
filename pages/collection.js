542aa8d91a38c531d23f87c8f4f9e9007207fa76
akashmadduru <akashmadduru@gmail.com>
2024-02-09 15:55:53 +0530

    Replacing react with react query and zustand

--- pages/collection.js
+++ pages/collection.js
@@ -1,26 +1,43 @@
-import { useEffect, useState } from "react";
+import Image from "next/image";
 
-import Mintbar from "../components/common/MintBar";
-import ContestCard from "../components/cards/ContestCard";
-import { colors } from "../theme/color";
+import DIAMOND from '../public/tickets/diamond.png'
+import GOLD from '../public/tickets/gold.png'
+import SILVER from '../public/tickets/silver.png'
+
+const images = [
+	{ img: GOLD, text: "Gold (5001 - 15000)" },
+	{ img: SILVER, text: "Silver (15000 - 50000)" },
+]
 
 const Collection = () => {
-	const isConnected = true;
 	return (
-		<>
-			<div className="" style={{ minHeight: "25vh" }}></div>
-			<div className="container" style={{ paddingBottom: "5rem" }}>
-				<div
-					style={{
-						display: "flex",
-						justifyContent: "center",
-						columnGap: "2rem",
-						alignItems: "center",
-					}}
-				>
+		<div
+			style={{
+				display: "flex",
+				flexDirection: 'column',
+				justifyContent: 'center',
+				alignItems: 'center',
+				paddingTop: "10rem",
+				paddingBottom: "4rem"
+			}}>
+			<h1>NFT's</h1>
+			<div className="container m-6">
+				<div style={{ display: "flex", justifyContent: 'center', alignItems: 'center' }}>
+					<div>
+						<Image src={DIAMOND} />
+						<h4 style={{ textAlign: 'center', marginTop: "1rem" }}>{`Diamond (1-5000)`}</h4>
+					</div>
+				</div>
+				<div className="row">
+					{images.map((i, k) =>
+						<div key={k} className="col-12 col-lg-6 my-4">
+							<Image src={i.img} />
+							<h4 style={{ textAlign: 'center', marginTop: "1rem" }}>{i.text}</h4>
+						</div>
+					)}
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
 
